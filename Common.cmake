c363103c63d90cc74b24d45ce9ece9669a4463d6
LionAnt <nickreal03@gmail.com>
2025-09-27 07:07:23 +0800

    wip

--- Common.cmake
+++ Common.cmake
@@ -108,10 +108,13 @@ set(DEP_SOURCE_DIR "${CMAKE_SOURCE_DIR}/dependencies/${DEP_NAME}")
 set(SHOULD_POPULATE TRUE)
 file(TO_NATIVE_PATH "${DEP_SOURCE_DIR}" DEP_DIR_NATIVE)
 
-file(MAKE_DIRECTORY "${DEP_DIR_NATIVE}\\cmake_test_dir")
-if(EXISTS "${DEP_DIR_NATIVE}\\cmake_test_dir" AND IS_DIRECTORY "${DEP_DIR_NATIVE}\\cmake_test_dir")
-  file(REMOVE "${DEP_DIR_NATIVE}\\cmake_test_dir")
+if(EXISTS "${DEP_DIR_NATIVE}" AND IS_DIRECTORY "${DEP_DIR_NATIVE}")
   set(SHOULD_POPULATE FALSE)
+else()
+  file(MAKE_DIRECTORY "${DEP_DIR_NATIVE}\\cmake_test_dir")
+  if(EXISTS "${DEP_DIR_NATIVE}\\cmake_test_dir" AND IS_DIRECTORY "${DEP_DIR_NATIVE}\\cmake_test_dir")
+    file(REMOVE "${DEP_DIR_NATIVE}\\cmake_test_dir")
+  endif()
 endif()
 
 
